9cf7a7d3716583d3a4cd558e678d5b1d59e3ee99
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2020-05-02 21:25:32 +0300

    Create a project

---
+++ Landmark/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmark
+//
+//  Created by Artyom on 5/2/20.
+//  Copyright © 2020 Artyom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello SwiftUI!")
+            .font(.largeTitle)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
